e4e90ea74e09183ec4415b5b0db961c1fafccce7
Robert Cunningham <rptc3000@gmail.com>
2024-06-27 11:45:46 -0400

    update header

--- website/src/app/components/nav.tsx
+++ website/src/app/components/nav.tsx
@@ -51,16 +51,22 @@ export function Nav() {
             <Box display="flex" alignItems="center" mb={1}>
               <Box
                 component="a"
-                href="https://www.instagram.com/a_heavy_heaven/"
-                aria-label="instagram"
+                href="https://www.linkedin.com/in/robertpcunningham/"
+                aria-label="LinkedIn"
                 target="_blank"
                 sx={{ display: "flex", mr: 2 }}
               >
-                <Box
-                  component="img"
-                  src="https://i.ibb.co/g6McB2g/Instagram-icon.png"
-                  width="24px"
-                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="rgb(10, 102, 194)"
+                  width="24"
+                  height="24"
+                  focusable="false"
+                >
+                  <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                </svg>
               </Box>
               <Box
                 component="a"
@@ -103,16 +109,22 @@ export function Nav() {
             </Link>
             <Box
               component="a"
-              href="https://www.instagram.com/a_heavy_heaven/"
-              aria-label="instagram"
+              href="https://www.linkedin.com/in/robertpcunningham/"
+              aria-label="LinkedIn"
               target="_blank"
               sx={{ display: "flex", mr: 2 }}
             >
-              <Box
-                component="img"
-                src="https://i.ibb.co/g6McB2g/Instagram-icon.png"
-                width="36px"
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="rgb(10, 102, 194)"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+              </svg>
             </Box>
             <Box
               component="a"
