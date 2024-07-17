fcdedbe93f7dcff0139e8136128fd62ed8b5fad3
Robert Cunningham <rptc3000@gmail.com>
2024-07-17 09:55:35 -0400

    work

--- website/src/app/features/pomodoro/main.tsx
+++ website/src/app/features/pomodoro/main.tsx
@@ -1,12 +1,29 @@
-import { Box, Typography } from "@mui/material";
-import dayjs from "dayjs";
+"use client";
+import { useState } from "react";
+
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
 
 import IntentionForm from "./intention-form";
 import Intentions from "./intentions";
 import StackedBarChartMonth from "./stacked-bar-chart-month";
+import { ChartTypes } from "./types";
 
 export default function Main() {
-  const now = dayjs();
+  const [chartType, setChartType] = useState<ChartTypes>("month");
+  const onChangeChartType: (
+    event: SelectChangeEvent<ChartTypes>,
+    child: React.ReactNode
+  ) => void = (e) => {
+    setChartType(e.target.value as ChartTypes);
+  };
 
   return (
     <>
@@ -21,7 +38,20 @@ export default function Main() {
       <Box mb={5}>
         <Intentions />
       </Box>
-      <StackedBarChartMonth />
+      <FormControl>
+        <InputLabel id="chart-type-select-label">Chart Type</InputLabel>
+        <Select<ChartTypes>
+          labelId="chart-type-select-label"
+          value={chartType}
+          onChange={onChangeChartType}
+          label="Chart Type"
+          MenuProps={{ disableScrollLock: true }}
+        >
+          <MenuItem value="month">Month</MenuItem>
+          <MenuItem value="date">Date</MenuItem>
+        </Select>
+      </FormControl>
+      {chartType === "month" && <StackedBarChartMonth />}
     </>
   );
 }
