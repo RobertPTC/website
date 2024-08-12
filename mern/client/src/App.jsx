79e2a6b8850cf54ca161c0f9ae2e1b039d6c271a
Robert Cunningham <rptc3000@gmail.com>
2024-08-12 18:40:47 -0400

    work

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,12 +1,38 @@
-import { Box } from '@mui/material'
+import { Box, Button } from "@mui/material";
+import { useRef } from "react";
 export default function App() {
+  const formRef = useRef(null);
+  async function onSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+    if (formRef.current) {
+      const body = new FormData(formRef.current);
+      const req = await fetch("http://localhost:5050/payments/upload-file", {
+        method: "POST",
+        body,
+      });
+      const json = await req.json()
+      console.log('req.json ', json)
+    }
+  }
+  /**
+   * @param {Event} e
+   */
+  function onFileChange(e) {
+    console.log('e ', e.target.files)
+  }
   return (
     <>
-      <Box component='form'>
-        <Box component='input' type="file" />
+      <Box component="form" onSubmit={onSubmit} ref={formRef}>
+        <Box component="input" type="file" name="paymentFile" accept=".xml" onChange={onFileChange}/>
+        <Box
+          component="input"
+          type="hidden"
+          name="companyID"
+          value="dunkinDonuts"
+        />
+        <Button type="submit">Upload</Button>
       </Box>
     </>
-  )
+  );
 }
-
-
--- mern/server/routes/payments.js
+++ mern/server/routes/payments.js
@@ -1,13 +1,158 @@
-import express from "express"
+import express from "express";
 import fileUpload from "express-fileupload";
+import { XMLParser as FastXMLParser } from "fast-xml-parser";
+import { v4 as uuidv4 } from "uuid";
 
-const router = express.Router()
+/**
+ * @typedef {Object} Employee
+ * @property {string} DunkinId
+ * @property {string} DunkinBranch
+ */
 
-router.use(fileUpload())
+/**
+ * @typedef {Object} Payor
+ * @property {string} DunkinId
+ * @property {number} AccountNumber
+ * @property {number} ABARouting
+ * @property {number} EIN
+ */
+
+/**
+ * @typedef {Object} Payee
+ * @property {string} PlaidId
+ * @property {number} LoanAccountNumber
+ */
+
+/**
+ * @typedef {Object} Row
+ * @property {Employee} Employee
+ * @property {Payor} Payor
+ * @property {Payee} Payee
+ * @property {string} Amount
+ */
+
+/**
+ * @function
+ * @name parse
+ * @param {string} xml
+ * @returns {Object}
+ */
+
+/**
+ * @typedef {Object} XMLParser
+ * @property {parse} parse Transforms xml string into Javascript object
+ */
+
+/**
+ *
+ * @param {string} amount
+ * @return number
+ */
+function dollarAmountToNumber(amount) {
+  return Number(amount.slice(1)) * 100;
+}
+/**
+ *
+ * @param {XMLParser} xmlParser
+ * @param {string} xml
+ * @return {Row[]}
+ */
+function parseXMLFile(xmlParser, xml) {
+  const parsed = xmlParser.parse(xml);
+  const {
+    root: { row },
+  } = parsed;
+  return row;
+}
+/**
+ *
+ * @param {{}} cache
+ * @param {string} uuid
+ * @param {Row[]} rows
+ */
+function putRowsInCache(cache, uuid, rows) {
+  cache[uuid] = rows;
+}
+
+// replace with a non-memory cache for production
+// should be an interface for dependency injection
+let cache = {};
+
+const router = express.Router();
+
+router.use(fileUpload());
 
 router.post("/upload-file", async (req, res) => {
-    console.log('req.file ', req.files)
-    res.status(200).send('ok')
+  const parser = new FastXMLParser();
+  const {
+    files: { paymentFile },
+  } = req;
+
+  const rows = parseXMLFile(parser, paymentFile.data.toString());
+  let employeeIDToEmployee = {};
+  let payorIDToPayor = {};
+  let fundsPerSourceAccount = {};
+  let fundsPerBranch = {};
+  let fundsPerEIN = {};
+  let total = 0;
+  rows.forEach((r) => {
+    const {
+      Employee,
+      Payor,
+      Payee: { LoanAccountNumber },
+      Amount,
+    } = r;
+    const { DunkinBranch, DunkinId } = Employee;
+    const { DunkinId: PayorDunkinId, AccountNumber, EIN } = Payor;
+    const amount = dollarAmountToNumber(Amount);
+    if (
+      !DunkinId ||
+      !DunkinBranch ||
+      !PayorDunkinId ||
+      !LoanAccountNumber ||
+      isNaN(amount)
+    ) {
+      return;
+    }
+    total += amount;
+    if (!fundsPerBranch[DunkinBranch]) {
+      fundsPerBranch[DunkinBranch] = {
+        amount,
+      };
+    } else {
+      fundsPerBranch[DunkinBranch].amount += amount;
+    }
+
+    if (!fundsPerSourceAccount[AccountNumber]) {
+      fundsPerSourceAccount[AccountNumber] = {
+        amount,
+      };
+    } else {
+      fundsPerSourceAccount[AccountNumber].amount += amount;
+    }
+
+    if (!fundsPerEIN[EIN]) {
+      fundsPerEIN[EIN] = {
+        amount,
+      };
+    } else {
+      fundsPerEIN[EIN].amount += amount;
+    }
+    employeeIDToEmployee[DunkinId] = Employee;
+    payorIDToPayor[PayorDunkinId] = Payor;
+  });
+  console.log("row[0] ", rows[0]);
+  console.log("row[1] ", rows[1]);
+  console.log("total ", total);
+  console.log("fundsPerSourceAccount ", fundsPerSourceAccount);
+  console.log("fundsPerBranch ", fundsPerBranch);
+  console.log("fundsPerEIN ", fundsPerEIN);
+  console.log("payorIDToPayor ", payorIDToPayor);
+  const cacheID = uuidv4();
+  putRowsInCache(cache, cacheID, rows);
+  res
+    .status(200)
+    .json({ total, fundsPerSourceAccount, fundsPerBranch, cacheID });
 });
 
 export default router;
