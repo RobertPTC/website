1a8c633b093345a4be7c14b9ba1e11e44735571b
Robert Cunningham <rptc3000@gmail.com>
2024-08-18 19:14:57 -0400

    blog work

--- website/src/app/(pages)/blog/previews.tsx
+++ website/src/app/(pages)/blog/previews.tsx
@@ -1,4 +1,5 @@
-//@ts-nocheck
+"use client";
+
 import {
   Box,
   Card,
@@ -10,23 +11,25 @@ import {
 
 import { blogPreviews } from "./blogs";
 
+type FileTreeNode = {
+  label: string;
+  children: FileTreeNode[];
+  path: string;
+};
+
 var paths = [
-  "About.vue",
-  "Categories/Index.vue",
-  "Categories/Demo.vue",
-  "Categories/Flavors.vue",
-  "Categories/Types/Index.vue",
-  "Categories/Types/Other.vue",
+  "engineering/building-the-google-timer.post",
+  "engineering/building-the-blog-file-directory.post",
 ];
 
 function buildFileTree(files: string[]) {
-  let result = [];
+  let result: FileTreeNode[] = [];
   files.forEach((element) => {
     let tmp = result;
-    element.split("/").forEach((e) => {
-      let i = tmp.find((o) => o.name === e);
+    element.split("/").forEach((e, idx) => {
+      let i = tmp.find((o) => o.label === e);
       if (!i) {
-        i = { name: e, children: [] };
+        i = { label: e, children: [], path: e };
         tmp.push(i);
       }
       tmp = i.children;
@@ -34,32 +37,69 @@ function buildFileTree(files: string[]) {
   });
   return result;
 }
-buildFileTree(paths);
+
+function h(root: HTMLElement, node: FileTreeNode) {
+  if (!node.children.length) {
+    const label = document.createElement("a");
+    label.setAttribute("data-path", node.path);
+    label.classList.add("leaf");
+    label.innerText = node.label;
+    root.append(label);
+    return;
+  }
+  let label = document.createElement("h2");
+  label.innerText = node.label;
+  label.classList.add("directory");
+  let container = document.createElement("div");
+  root.append(label);
+  root.append(container);
+  node.children.forEach((c) => {
+    h(container, c);
+  });
+}
+
+function buildDirectoryDOM(graph: FileTreeNode[]) {
+  if (typeof window !== "undefined") {
+    let rootNode = document.createElement("div");
+    graph.forEach((node) => {
+      let r = rootNode;
+      h(r, node);
+    });
+    return rootNode;
+  }
+  return document.createElement("div");
+}
 
 export default function BlogPreviews() {
   return (
-    <Grid container>
-      {JSON.stringify(buildFileTree(paths)[1].children)}
-      {blogPreviews.map(({ title, previewText, date, imgURL }) => (
-        <Grid item xs={4} key={title}>
-          <Card variant="outlined">
-            <CardActionArea>
-              <CardContent>
-                <Typography variant="h2" sx={{ fontSize: "35px", mb: 1 }}>
-                  {title}
-                </Typography>
-                <Typography
-                  variant="h3"
-                  sx={{ fontSize: "21px", fontWeight: 100 }}
-                >
-                  {previewText}
-                </Typography>
-                <Box component="img" src={imgURL} />
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: buildDirectoryDOM(buildFileTree(paths)).innerHTML,
+        }}
+      />
+      <Grid container>
+        {blogPreviews.map(({ title, previewText, date, imgURL }) => (
+          <Grid item xs={4} key={title}>
+            <Card variant="outlined">
+              <CardActionArea>
+                <CardContent>
+                  <Typography variant="h2" sx={{ fontSize: "35px", mb: 1 }}>
+                    {title}
+                  </Typography>
+                  <Typography
+                    variant="h3"
+                    sx={{ fontSize: "21px", fontWeight: 100 }}
+                  >
+                    {previewText}
+                  </Typography>
+                  <Box component="img" src={imgURL} />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
