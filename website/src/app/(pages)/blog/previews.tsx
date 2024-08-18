46d6ebe7361ef59337f445e00e365c937e4c6009
Robert Cunningham <rptc3000@gmail.com>
2024-08-18 00:05:14 -0400

    build file tree algo

--- website/src/app/(pages)/blog/previews.tsx
+++ website/src/app/(pages)/blog/previews.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import {
   Box,
   Card,
@@ -9,9 +10,36 @@ import {
 
 import { blogPreviews } from "./blogs";
 
+var paths = [
+  "About.vue",
+  "Categories/Index.vue",
+  "Categories/Demo.vue",
+  "Categories/Flavors.vue",
+  "Categories/Types/Index.vue",
+  "Categories/Types/Other.vue",
+];
+
+function buildFileTree(files: string[]) {
+  let result = [];
+  files.forEach((element) => {
+    let tmp = result;
+    element.split("/").forEach((e) => {
+      let i = tmp.find((o) => o.name === e);
+      if (!i) {
+        i = { name: e, children: [] };
+        tmp.push(i);
+      }
+      tmp = i.children;
+    });
+  });
+  return result;
+}
+buildFileTree(paths);
+
 export default function BlogPreviews() {
   return (
     <Grid container>
+      {JSON.stringify(buildFileTree(paths)[1].children)}
       {blogPreviews.map(({ title, previewText, date, imgURL }) => (
         <Grid item xs={4} key={title}>
           <Card variant="outlined">
