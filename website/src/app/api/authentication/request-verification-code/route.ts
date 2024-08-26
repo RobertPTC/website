e644b931629084ab292270f112aef77ce8c8dbe7
Robert Cunningham <rptc3000@gmail.com>
2024-08-26 14:13:40 -0400

    fix build

--- website/src/app/api/authentication/request-verification-code/route.ts
+++ website/src/app/api/authentication/request-verification-code/route.ts
@@ -1,34 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { v4 as uuid } from "uuid";
 
-import EmailService, { emailService, validEmailRegex } from "@app/email";
-import MemoryCache, { memoryCache } from "@app/memory-cache";
+import { emailService } from "@app/email";
+import { memoryCache } from "@app/memory-cache";
 
-export async function requestVerificationCode(
-  req: Request,
-  memoryCache: MemoryCache,
-  emailService: EmailService
-): Promise<NextResponse> {
-  try {
-    const json = await req.json();
-
-    if (!validEmailRegex.test(json.email)) {
-      return NextResponse.json({ status: 400, statusText: "invalid email" });
-    }
-    const token = uuid();
-    const res = await memoryCache.setVerificationToken(
-      json.email,
-      token,
-      5 * 60
-    );
-    if (res) {
-      return NextResponse.json({ status: 200, statusText: "OK" });
-    }
-    return NextResponse.json({ status: 500, statusText: "key not set" });
-  } catch (e) {
-    return NextResponse.json({ status: 500, statusText: "key not set" });
-  }
-}
+import { requestVerificationCode } from "..";
 
 export async function POST(req: NextRequest) {
   return requestVerificationCode(req, memoryCache, emailService);
