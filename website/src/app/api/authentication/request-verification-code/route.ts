14efd8e4009d2b5d504b27986050f56520dd14ab
Robert Cunningham <rptc3000@gmail.com>
2024-08-26 14:13:58 -0400

    clean up

--- website/src/app/api/authentication/request-verification-code/route.ts
+++ website/src/app/api/authentication/request-verification-code/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 import { emailService } from "@app/email";
 import { memoryCache } from "@app/memory-cache";
