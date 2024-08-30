86ea12ebbce4cb40abea122e919d21cb47893a2e
Robert Cunningham <rptc3000@gmail.com>
2024-08-30 13:36:19 -0400

    comments

---
+++ website/src/db/migrations/migration-4.sql
@@ -0,0 +1,9 @@
+CREATE TABLE blog_comment(
+    blog_comment_id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    date TEXT NOT NULL,
+    text TEXT NOT NULL,
+    blog_id UUID NOT NULL,
+    responds_to UUID NOT NULL, 
+    journalist_id UUID NOT NULL REFERENCES journalist (journalist_id)
+);
