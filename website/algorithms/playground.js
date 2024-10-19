dfe7bc609a4cb2df06149648263d11f6628707ca
Robert Cunningham <rptc3000@gmail.com>
2024-10-19 13:13:05 -0400

    checkpoint

--- website/algorithms/playground.js
+++ website/algorithms/playground.js
@@ -227,20 +227,72 @@ function controlSetTimeout() {
   }, timeoutDelay);
 }
 
+let nextId = 0;
+
+function tryDriftless(id, opts) {
+  const { atMs, fn, thresholdMs = 1, aggression = 1.1 } = opts;
+  const delayMs = atMs - Date.now();
+  const handle =
+    delayMs > thresholdMs
+      ? setTimeout(() => {
+          tryDriftless.apply(this, arguments); // eslint-disable-line prefer-rest-params
+        }, delayMs / aggression)
+      : setTimeout(() => {
+          // Call the function using setTimeout to ensure asynchrony
+          fn();
+        }, 0);
+}
+
+function setDriftless(opts) {
+  const id = nextId;
+  nextId += 1;
+  tryDriftless(id, opts);
+  return id;
+}
+
+function setDriftlessInterval(fn, delayMs, ...params) {
+  let id;
+  const opts = {
+    atMs: Date.now() + delayMs,
+    fn(...args) {
+      opts.atMs += delayMs;
+      tryDriftless(id, opts);
+      return fn.call(this, ...args, ...params);
+    },
+  };
+  id = setDriftless(opts);
+  return id;
+}
+
+function backoff(fn) {
+  if (fn.__next - Date.now() > 1) {
+    setTimeout(() => {
+      backoff(fn);
+    }, (fn.__next - Date.now()) / 1.1);
+    return;
+  }
+  setTimeout(() => {
+    fn();
+  });
+  attunedSetTimeout(1000, fn);
+}
+
 function attunedSetTimeout(delay, fn) {
   const start = Date.now();
-  const id = setTimeout(() => {
-    const end = Date.now();
-    const span = end - start;
-    const newDelay = timeoutDelay * 2 - span;
-    console.log("newDelay is greater than 1000 ", newDelay > timeoutDelay);
-
-    fn();
-    attunedSetTimeout(newDelay, fn);
-  }, delay);
+  fn.__next = start + delay;
+  const lapse = fn.__next - Date.now();
+  if (lapse > 1) {
+    setTimeout(() => {
+      backoff(fn);
+    }, lapse / 1.1);
+  }
 }
 
 attunedSetTimeout(1000, () => {
   console.log("attuned ", Date.now() - start);
 });
+
+setDriftlessInterval(() => {
+  console.log("driftless ", Date.now() - start);
+}, 1000);
 controlSetTimeout();
